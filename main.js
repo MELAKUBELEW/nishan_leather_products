d74f9654973fbe521ea94ff4d9773ba2a004a9a9
MELAKUBELEW <140408368+MELAKUBELEW@users.noreply.github.com>
2023-07-25 13:42:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,191 @@
+var menu = document.querySelector('#menu')
+var clear = document.querySelector('#clear')
+var navbar = document.querySelector('#side_nav')
+if(menu){
+    menu.addEventListener('click',function(){
+        menu.style.display = "none";
+        navbar.style.display = "block";
+    })
+}
+if(clear){
+    clear.addEventListener('click',function(){
+        navbar.style.display = "none";
+        menu.style.display = "flex";
+    })
+}
+
+
+var products = document.getElementById("products1")
+var prodetailf1 = document.getElementById("prodetail_onef")
+var prodetailf2 = document.getElementById("prodetail_twof")
+var prodetailf3= document.getElementById("prodetail_threef")
+var prodetailf4= document.getElementById("prodetail_fourf")
+var prodetailf5 = document.getElementById("prodetail_fivef")
+var prodetailf6 = document.getElementById("prodetail_sixf")
+var prodetailf7 = document.getElementById("prodetail_sevenf")
+var prodetailf8 = document.getElementById("prodetail_eightf")
+var prodetailm1 = document.getElementById("prodetail_onem")
+var prodetailm2 = document.getElementById("prodetail_twom")
+var prodetailm3= document.getElementById("prodetail_threem")
+var prodetailm4= document.getElementById("prodetail_fourm")
+var prodetailm5 = document.getElementById("prodetail_fivem")
+var prodetailm6 = document.getElementById("prodetail_sixm")
+var prodetailm7 = document.getElementById("prodetail_sevenm")
+var prodetailm8 = document.getElementById("prodetail_eightm")
+var fimg1 = document.getElementById("fimg_one")
+var fimg2 = document.getElementById("fimg_two")
+var fimg3 = document.getElementById("fimg_three")
+var fimg4 = document.getElementById("fimg_four")
+var fimg5 = document.getElementById("fimg_five")
+var fimg6 = document.getElementById("fimg_six")
+var fimg7 = document.getElementById("fimg_seven")
+var fimg8 = document.getElementById("fimg_eight")
+var mimg1 = document.getElementById("mimg_one")
+var mimg2 = document.getElementById("mimg_two")
+var mimg3 = document.getElementById("mimg_three")
+var mimg4 = document.getElementById("mimg_four")
+var mimg5 = document.getElementById("mimg_five")
+var mimg6 = document.getElementById("mimg_six")
+var mimg7 = document.getElementById("mimg_seven")
+var mimg8 = document.getElementById("mimg_eight")
+var newsletter = document.getElementById("newsletter")
+var shop_container = document.getElementById("shop-container")
+
+
+if (fimg1){
+    fimg1.addEventListener("click",function(){
+        prodetailf1.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+
+    })
+}
+if (fimg2){
+    fimg2.addEventListener("click",function(){
+        prodetailf2.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg3){
+    fimg3.addEventListener("click",function(){
+        prodetailf3.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg4){
+    fimg4.addEventListener("click",function(){
+        prodetailf4.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg5){
+    fimg5.addEventListener("click",function(){
+        prodetailf5.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg6){
+    fimg6.addEventListener("click",function(){
+        prodetailf6.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg6){
+    fimg6.addEventListener("click",function(){
+        prodetailf6.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg7){
+    fimg7.addEventListener("click",function(){
+        prodetailf7.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (fimg8){
+    fimg8.addEventListener("click",function(){
+        prodetailf8.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg1){
+    mimg1.addEventListener("click",function(){
+        prodetailm1.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg2){
+    mimg2.addEventListener("click",function(){
+        prodetailm2.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg3){
+    mimg3.addEventListener("click",function(){
+        prodetailm3.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg4){
+    mimg4.addEventListener("click",function(){
+        prodetailm4.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg5){
+    mimg5.addEventListener("click",function(){
+        prodetailm5.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg6){
+    mimg6.addEventListener("click",function(){
+        prodetailm6.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg7){
+    mimg7.addEventListener("click",function(){
+        prodetailm7.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
+if (mimg8){
+    mimg8.addEventListener("click",function(){
+        prodetailm8.style.display = "flex";
+        products.style.display = "none";
+        newsletter.style.display = "none";
+        shop_container.style.display = "none";
+    })
+}
